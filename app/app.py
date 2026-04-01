b1fe9ea62460c98a704c13643a6c091dce71a882
Arkhipov Artem 231-3210 <artem57638@gmail.com>
2026-04-01 23:24:35 +0300

    fix app.py

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ db_user = os.environ.get('DB_USER', 'my_user')
 db_pass = os.environ.get('DB_PASS', 'pass123')
 db_host = os.environ.get('DB_HOST', 'db')
 db_name = os.environ.get('DB_NAME', 'my_database')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
